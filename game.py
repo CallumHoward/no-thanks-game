968883e0db279cefc38801f2bb72b57c4185bd07
CallumHoward <callum.howard@safetyculture.io>
2022-04-09 13:08:05 +1000

    Add scoring

--- game.py
+++ game.py
@@ -4,24 +4,30 @@ from itertools import cycle
 from player import Player
 
 root = logging.getLogger()
-root.setLevel(logging.DEBUG)
-logging.basicConfig(format="%(relativeCreated)04d:%(module)s:%(message)s")
+logging.basicConfig(format="[%(relativeCreated)04d:%(module)s] %(message)s")
 
 num_players = 4
 all_cards = list(range(3, 35))
 num_cards_to_remove = 9
 
+default_agent_names = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David"
+]
 
 def rotate(list, offset):
     return list[offset:] + list[:offset]
 
 
-def play() -> None:
+def play(agent_names: list[str] = default_agent_names, log_level = logging.INFO) -> str:
+    root.setLevel(log_level)
     logging.info("Setting up")
     deck = all_cards
     shuffle(deck)
     deck = deck[:num_cards_to_remove]
-    players = list(Player(player_number) for player_number in range(num_players))
+    players = list(Player(i, agent_names[i]) for i in range(num_players))
 
     current_player = choice(players)
     pot = 0
@@ -33,20 +39,28 @@ def play() -> None:
 
         for player in cycle(rotate(players, current_player.id)):
             current_player = player
-            logging.info(f"Current player: {current_player.id}")
-            logging.info(f"Pot: {pot}")
+            logging.info(f"Current player: {current_player.id}, Pot: {pot}, Card: {dealt_card}")
             other_players = [player for player in players if player != current_player]
 
             if current_player.should_take(dealt_card, pot, other_players, len(deck)):
-                logging.info(f"Player {current_player.id} takes {dealt_card}")
+                logging.info(f"{current_player.identify()} takes {dealt_card}")
                 current_player.take_card(dealt_card, pot)
                 pot = 0
                 break
             else:
-                logging.info(f"Player {current_player.id} does not take {dealt_card}")
+                logging.info(f"{current_player.identify()} does not take {dealt_card}")
                 current_player.not_take_card()
                 pot += 1
 
+    logging.info("Game finished")
+    scores = sorted(((player, player.score()) for player in players), key=lambda x: x[1])
+    for (player, score) in scores:
+        logging.info(f"{player.identify()} scored {score}")
+
+    winner = scores[0][0].name
+    logging.info(f"Winner was {winner}")
+
+    return winner
 
 if __name__ == "__main__":
     play()
--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 import logging
 
+def lowest_consecutive(list: list[int]) -> list[int]:
+    return [item for i, item in enumerate(sorted(list)) if i != len(list) - 1 and item + 1 != list[i + 1]]
 
 class Player:
-    def __init__(self, player_number):
+    def __init__(self, player_number: int, name: str):
         self.id = player_number  # Starting from 0
+        self.name = name
         self.coins = 11
-        self.played_cards = []
+        self.played_cards: list[int] = []
 
     def must_take(self) -> bool:
         return self.coins <= 0
@@ -17,8 +20,17 @@ class Player:
 
     def take_card(self, card, pot) -> None:
         self.played_cards.append(card)
+        self.played_cards.sort()
+        logging.info(f"Player {self.id} has {self.played_cards} cards")
         self.coins += pot
+        logging.info(f"Player {self.id} has {self.coins} coins")
 
     def not_take_card(self) -> None:
         logging.info(f"Player {self.id} has {self.coins} coins")
         self.coins -= 1
+
+    def score(self) -> int:
+        return sum(lowest_consecutive(self.played_cards)) - self.coins
+
+    def identify(self) -> str:
+        return f"Player {self.id} ({self.name})"
