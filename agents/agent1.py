8d3522661998dca8701b26f754fa00ed03853e2c
CallumHoward <callum.howard@safetyculture.io>
2022-07-28 19:06:27 +1000

    Rename agent

--- agents/agent1.py
+++ agents/agent1.py
@@ -4,7 +4,7 @@ import logging
 
 class Agent:
     def __init__(self) -> None:
-        self.name = "Charlie"
+        self.name = "Alice"
 
     def should_take(self, me, card, pot, other_players, deck_size) -> bool:
         if me.must_take():
